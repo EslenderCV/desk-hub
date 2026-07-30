f761b5734761be71b2fdac73d941e3087db01d68
EslenderCV <eslendercruz45@gmail.com>
2026-07-30 18:46:38 -0400

    feat: initial desk hub setup with docker, nginx, and pi-hole config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  // Optional: If using <Image /> component without server optimization
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
